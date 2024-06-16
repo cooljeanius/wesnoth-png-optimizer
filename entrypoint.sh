80462d988a5ad25a69b2869d49e9b7377d321993
Eric Gallager <egall@gwmail.gwu.edu>
2024-06-16 08:22:25 -0400

    (#6) Script refactoring.
    silence shellcheck output, and a few other minor style tweaks

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
 
 branch="${3:-master}"
-echo "Cloning Wesnoth $branch."
+echo "Cloning Wesnoth ${branch}."
 git clone \
     --depth 1 \
     --filter=blob:none \
     --sparse \
-    --single-branch --branch $branch \
+    --single-branch --branch "${branch}" \
     -c advice.detachedHead=false \
     https://github.com/wesnoth/wesnoth \
     /wesnoth
@@ -16,20 +16,20 @@ echo "Wesnoth repository is ready."
 # Capturing Python script stdout.
 exec 5>&1
 
-echo
+echo ""
 echo "Running woptipng with a threshold of $2..."
 output=$(
-    python /wesnoth/utils/woptipng.py -d $1 --threshold $2 2>&1 | \
+    python /wesnoth/utils/woptipng.py -d "$1" --threshold "$2" 2>&1 | \
     grep --line-buffered -P '^(optimized.*)|(not replacing.*)|(Nothing optimized.*)|(\d+ of \d+ files optimized.*)$' | \
     tee >(cat - >&5)
 )
 
 if printf "%s\n" "${output[@]}" | grep -Fxq "Nothing optimized"; then
-    echo
+    echo ""
     echo "All images are optimized within the given threshold."
     exit 0
 else
-    echo
+    echo ""
     echo "Found unoptimized images above the optimization threshold."
     echo "See the logs from the woptipng tool."
     exit 1
